49279dd20603b1745e5f7bd9ff6dcd687833a378
malongob <mrclngbrd@gmail.com>
2019-08-17 11:00:04 +0200

    First release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.modified.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-app';
+  name = 'Marco';
 }
